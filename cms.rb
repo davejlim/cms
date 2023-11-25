22c5251d1b9050a476885cf42d7d132a3e94432a
davejlim <7742500+davejlim@users.noreply.github.com>
2023-11-25 20:38:07 +0000

    Added editing functionality and success mesasge

--- cms.rb
+++ cms.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
+require 'redcarpet'
 
 configure do 
   enable :sessions
@@ -15,6 +16,21 @@ before do
   end
 end
 
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  when ".md"
+    render_markdown(content)
+  end
+end
 
 get '/' do
 
@@ -25,10 +41,26 @@ get '/:filename' do
   file_path = @root + "/data/" + params[:filename]
 
   if @files.include?("#{params[:filename]}")
-    headers["Content-Type"] = "text/plain"
-    File.read(file_path)
+    load_content(file_path)
   else
-    session[:error] = "#{params[:filename]} does not exist."
+    session[:message] = "#{params[:filename]} does not exist."
     redirect '/'
   end
+end
+
+get '/:filename/edit' do
+  @filename = params[:filename]
+  file_path = @root + "/data/" + params[:filename]
+  @content = File.read(file_path)
+
+  erb :edit
+end
+
+post '/:filename/edit' do
+  file_path = @root + "/data/" + params[:filename]
+
+  File.write(file_path, params[:content])
+
+  session[:message] = "#{params[:filename]} has been successfully updated."
+  redirect "/"
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -16,7 +16,7 @@ class CMSTest < Minitest::Test
     get "/"
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
     assert_includes last_response.body, "history.txt"
   end
@@ -38,5 +38,35 @@ class CMSTest < Minitest::Test
 
     get "/"
     refute_includes last_response.body, "fake.text does not exist."
+  end
+
+  def test_markdown
+    get "/about.md"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>Ruby is...</h1>"
+  end
+
+  def test_editing_document
+    get "/changes.txt/edit"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<textarea"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
+
+  def test_updating_document
+    post "/changes.txt", content: "new content"
+
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+
+    assert_inclues last_response.body, "changes.txt has been updated"
+
+    get "/changes.txt"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "new content"
   end
 end
